fbb1204ede537c9dda7a90f00ce6ae255f365065
phamngtuananh <phamngtuananh@gmail.com>
2018-03-10 00:08:35 +0800

    Fix bug

--- merlion_perception/nodes/mission_planner.py
+++ merlion_perception/nodes/mission_planner.py
@@ -20,6 +20,10 @@ import random
 #################################
 
 class Mission(object):
+    switch_init = False # wait for armed state from estop
+    last_disarm_time = 0
+    arming_count = 0
+
     # sleeping time
     timestep = 0.1
 
@@ -63,6 +67,16 @@ class Mission(object):
         rospy.init_node(nodename, anonymous=False)
         self.bridge = CvBridge()
 
+        #### A Subscriber to switch: wait for 2 secs of armed state before initializing
+        self.last_disarm_time = rospy.get_time()
+        rospy.loginfo('Waiting for armed...')
+        waitInitSub = rospy.Subscriber("/merlion/disarm", Bool, self.wait_init, queue_size=1)
+        while not rospy.is_shutdown() and not self.switch_init:
+            rospy.sleep(0.5)
+        rospy.loginfo('Received arming, initialize!')
+        time.sleep(5)
+        waitInitSub.unregister()
+
         ####Subscribers####
 
         #sub to heatmaps from detector
@@ -105,6 +119,16 @@ class Mission(object):
                 self.mission_2()
             elif i == 3:
                 self.mission_3()
+
+    def wait_init(self, msg):
+        if msg.data == True: # disarmed
+            self.last_disarm_time = rospy.get_time()
+            self.arming_count = 0
+        else:
+            self.arming_count += 1
+
+        if rospy.get_time() - self.last_disarm_time > 2.0 and self.arming_count > 10: # armed consistently!
+            self.switch_init = True
 
     def mission_0(self):
         ####qualification####
